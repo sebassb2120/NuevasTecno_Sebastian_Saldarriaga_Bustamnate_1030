2e4bd6e3893953df211aaeb23186e74ca3b5c7ad
Sebastian Saldarriaga Bustamante <sebassaldarriaga20@gmail.com>
2023-09-02 11:29:35 -0500

    Update Listas.py
    se agrega juego

--- Apuntes/Listas.py
+++ Apuntes/Listas.py
@@ -1,1 +1,20 @@
-usua =  {"id": 1, "email": "sebas@gmail.com", "phone": "2307699402", "password": "sebas123"},
+usua =  {"id": 1, "email": "sebas@gmail.com", "phone": "2307699402", "password": "sebas123"}
+
+# Jueguito ejercicio
+import random
+
+vidas = 5
+
+puntos = 0
+
+while(vidas != 0):
+
+    num=random.randint(0,9)
+
+    if num == 0:
+        vidas -=1
+        print(f'vidas: {vidas}')
+    
+    else:
+        puntos +=1
+        print(f"puntos: {puntos}")
